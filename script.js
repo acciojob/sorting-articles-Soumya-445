1fa6c269accea43aad0218038c95fdc60a2217ab
Soumya Manasa Basantaray <soumyamanasa2003@gmail.com>
2026-02-12 22:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+// Function to remove articles for sorting
+function stripArticle(bandName) {
+  return bandName.replace(/^(a |an |the )/i, '').trim();
+}
+
+// Sort ignoring articles
+const sortedBands = bands.sort((a, b) => {
+  const bandA = stripArticle(a);
+  const bandB = stripArticle(b);
+  return bandA.localeCompare(bandB);
+});
+
+// Display in UL
+const ul = document.getElementById('band');
+
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
